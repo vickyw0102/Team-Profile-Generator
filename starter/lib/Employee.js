c1873aeb4c3d930385db695932bc707a758f7fdc
vicky wong <vicky.boci@gmail.com>
2024-02-24 04:10:34 +0800

    Draft HTML and employee js

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,6 @@
 // TODO: Write code to define and export the Employee class
+const inquirer = require("inquirer");;
+const fs = require("fs");
+const engineer = require("./Engineer");
+const intern = require("./Intern");
+const manager = require("./Manager");
