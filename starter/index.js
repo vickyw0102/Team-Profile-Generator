4dfabd209009831cc4e8089c74c0b51baf042e84
vicky wong <vicky.boci@gmail.com>
2024-02-28 20:10:43 +0800

    modified index.js and all js files

--- starter/index.js
+++ starter/index.js
@@ -1,5 +1,6 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
+const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
@@ -12,4 +13,103 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+let team=[];
 
+//create  prompt to enter the team manager’s info
+const managerInfo = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the team manager's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the team manager's id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the team manager's email?"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the team manager's office number?"
+    }
+];
+
+// create a menu with the option
+const menu = [
+    {
+        type: "list",
+        name: "menu",
+        message: "What would you like to do?",
+        choices: ["Add an engineer", "Add an intern", "Finish building my team"]
+    }
+];
+
+//prompt the engineer questions to get engineer info
+const engineerInfo = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's github username?"
+    }
+];
+
+//promopt the intern questions to get intern info
+const internInfo = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the intern's id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email?"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is the intern's school?"
+    }
+];
+const EmployeeInfo = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email?"
+    }
+];
